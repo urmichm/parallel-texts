95ad3d191c291cabbdfc5439f130f3a20b35da9e
Michael <urmich.m@gmail.com>
2023-04-19 20:47:15 +0200

    footer to use semantic ui

--- view/footer.php
+++ view/footer.php
@@ -1,3 +1,5 @@
-<footer >
-    <p>&copy; 2023 <?php echo $PAGE_TITLE; ?></p>    
-</footer>
+<div class="ui inverted vertical footer segment">
+    <div class="ui center aligned container">
+         <h4 class="ui inverted header">&copy; 2023 <?php echo $PAGE_TITLE; ?></h4>
+    </div>
+</div>
